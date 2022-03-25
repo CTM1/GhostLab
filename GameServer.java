87a5932645bc4288a5f5758019ebdff4963771f2
krkn <claudio.t1337@gmail.com>
2022-03-25 17:51:22 +0100

    First server classes

---
+++ GameServer.java
@@ -0,0 +1,14 @@
+protected class GameServer {
+    int id;
+    Player[] players;
+    Character[][] labyrinth;
+    Ghost[] ghosts;
+
+    public GameServer(String id, int port) {
+        // Make ghosts players and shit
+    }
+
+    public void joinGame(String id) {
+        // Make new player, drop into labyrinth
+    }
+}
---
+++ Ghosts.java
@@ -0,0 +1,13 @@
+public class Ghost {
+    int x;
+    int y;
+
+    public Ghost(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public moveGhost(Character[][] labyr) {
+        System.out.println("prout");
+    }
+}
