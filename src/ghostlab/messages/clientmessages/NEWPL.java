19f55acdd17a885d624fbc39dc91fb7c85cf9220
DaRealNim <darealnim@gmail.com>
2022-04-06 01:29:55 +0200

    made static builder parse() for NEWPL

--- src/ghostlab/messages/clientmessages/NEWPL.java
+++ src/ghostlab/messages/clientmessages/NEWPL.java
@@ -7,24 +7,34 @@ public class NEWPL {
     private String playerID; // 8 chars or fuck off!
     private String port; // 4 chars or fuck off!
 
-    public NEWPL(BufferedReader br) throws IOException {
-        //this.playerID = i;
-        //this.port = p;
+    public NEWPL(String playerID, String port) throws IOException {
+        this.playerID = playerID;
+        this.port = port;
+        
+    }
+
+    public static NEWPL parse(BufferedReader br) throws IOException {
         br.read(); // the space
-        this.playerID = "";
-        this.port = "";
+        String playerID = "";
+        String port = "";
         String suffix = "";
 
         for(int i=0; i<8; i++) //the player id
-            this.playerID += (char)br.read();
+            playerID += (char)br.read();
 
         br.read(); // the space
 
         for(int i=0; i<4; i++) //the port
-            this.port += (char)br.read();
+            port += (char)br.read();
 
         for(int i=0; i<3; i++) //***
             suffix += (char)br.read();
+
+        return new NEWPL(playerID, port);
+    }
+
+    public String toString() {
+        return "NEWPL "+this.playerID+" "+this.port+"***";
     }
 
     public String getPlayerID() {
