5ed6bf70e9c2c3053b0cae7b75d5ab3fffdf3abe
DaRealNim <darealnim@gmail.com>
2022-04-08 12:44:01 +0200

    started client

---
+++ client/logo.c
@@ -0,0 +1,8 @@
+char logoline1[] = "  ________.__                    __  .____          ___.    ";
+char logoline2[] = " /  _____/|  |__   ____  _______/  |_|    |   _____ \\_ |__  ";
+char logoline3[] = "/   \\  ___|  |  \\ /  _ \\/  ___/\\   __\\    |   \\__  \\ | __ \\ ";
+char logoline4[] = "\\    \\_\\  \\   Y  (  <_> )___ \\  |  | |    |___ / __ \\| \\_\\ \\";
+char logoline5[] = " \\______  /___|  /\\____/____  > |__| |_______ (____  /___  /";
+char logoline6[] = "        \\/     \\/           \\/               \\/    \\/    \\/ ";
+
+char *logolines[] = {logoline1, logoline2, logoline3, logoline4, logoline5, logoline6};
