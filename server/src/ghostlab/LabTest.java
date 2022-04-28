11194a8ff968f66b629444ba793d54eeda6b6ca7
DaRealNim <darealnim@gmail.com>
2022-04-28 19:32:52 +0200

    finished lobby, bug fixing

---
+++ server/src/ghostlab/LabTest.java
@@ -0,0 +1,13 @@
+package ghostlab;
+
+public class LabTest {
+    public static void main(String[] args) {
+        Labyrinth lab = new Labyrinth(80, 100);
+        for (boolean[] row : lab.getSurface()) {
+            for(boolean c : row) {
+                System.out.print(c ? " " : "#");
+            }
+            System.out.println();
+        }
+    }
+}
