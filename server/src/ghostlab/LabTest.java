70c37df614961bf22750bcd194bdec7c865921b5
Olivier Moreau <m242@pm.me>
2022-05-08 23:37:22 +0200

    New maze just dropped

--- server/src/ghostlab/LabTest.java
+++ server/src/ghostlab/LabTest.java
@@ -1,19 +1,16 @@
 package ghostlab;
 
 public class LabTest {
-	public static void main(String[] args) {
-		Maze x = new Maze(50,50);
-		boolean[][] xx =  x.getSurface();
-		for(boolean[] l: xx) {
-			for (boolean z:l) {
-				if(z) {
-					System.out.print("1");
-				} else {
-					System.out.print("0");
-				}
-			}
-			System.out.println();
-		}
-		System.out.format("Dimensions: %d:%d\n", xx.length, xx[0].length);
-	}
+  public static void main(String[] args) {
+    Maze x = new Maze(120, 120);
+
+    boolean[][] xx = x.getSurface();
+
+    for (boolean[] l : xx) {
+      for (boolean c : l) {
+        System.out.print(c ? " " : "x");
+      }
+      System.out.println();
+    }
+  }
 }
