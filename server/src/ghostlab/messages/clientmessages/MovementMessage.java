cc7e4811a0793c6b1097dc26842e26e94580615e
Olivier Moreau <m242@pm.me>
2022-05-09 00:00:04 +0200

    wip

---
+++ server/src/ghostlab/messages/clientmessages/MovementMessage.java
@@ -0,0 +1,18 @@
+package ghostlab.messages.clientmessages;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class MovementMessage {
+  /** Used by movement message parsers to get the distance. Boilerplate code */
+  public static int parseDistance(BufferedReader br) throws IOException {
+    br.read(); // space
+    String dist = "";
+    for (int i = 0; i < 3; i++) {
+      dist += (char) br.read();
+    }
+
+    int d = Integer.parseInt(dist);
+    return d;
+  }
+}
