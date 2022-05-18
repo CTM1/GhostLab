9db8bb6233dd1dcf81ebdb22f692807adc62f3aa
DaRealNim <darealnim@gmail.com>
2022-05-18 11:59:19 +0200

    fixed recursivemaze not being a connected graph, minor changes

--- server/src/ghostlab/GameServer.java
+++ server/src/ghostlab/GameServer.java
@@ -167,11 +167,12 @@ public class GameServer {
         } catch (Exception e) {
           Logger.log("%d : Invalid message from player %s", daddy.getGameId(), playa.getPlayerID());
           e.printStackTrace();
+          return;
         }
       }
     }
 
-    private void testMoveAndSendBackMOVEF(int direction, int distance) {
+    private synchronized void testMoveAndSendBackMOVEF(int direction, int distance) {
       ArrayList<Ghost> realMFGs = daddy.getGhosts();
       boolean[][] maze = daddy.labyrinth.getSurface();
 
@@ -204,6 +205,7 @@ public class GameServer {
 
         moved++;
         // check for ghosts
+        ArrayList<Ghost> toRemove = new ArrayList<Ghost>();
         for (Ghost g : realMFGs) {
           if (position[0] == g.getX() && position[1] == g.getY()) {
             // break the move
@@ -226,8 +228,12 @@ public class GameServer {
             }
 
             // remove ghost
-            realMFGs.remove(g);
+            Logger.log("Caught a ghost!");
+            toRemove.add(g);
           }
+        }
+        for (Ghost g : toRemove) {
+          realMFGs.remove(g);
         }
       }
 
--- server/src/ghostlab/RecursiveMaze.java
+++ server/src/ghostlab/RecursiveMaze.java
@@ -2,155 +2,168 @@ package ghostlab;
 
 import java.util.Random;
 
+
+
 public class RecursiveMaze implements LabyrInterface {
 
-  private boolean grid[][];
-  private int width;
-  private int height;
-  private Random rand = new Random();
+    private boolean grid[][];
+    private int width;
+    private int height;
+    private Random rand = new Random();
 
-  private void recursiveChamber(boolean grid[][], int startX, int startY, int endX, int endY) {
-
-    int chamberSizeX = endX - startX + 1;
-    int chamberSizeY = endY - startY + 1;
-    if (chamberSizeX == 2 && chamberSizeY == 2) return;
-    if (chamberSizeX < 2 || chamberSizeY < 2) {
-      // System.out.println(String.format("Aborting (%d, %d) to (%d, %d)", startX, startY, endX,
-      // endY));
-      return;
+    private int randInRange(int min, int max) {
+      return rand.nextInt((max - min)) + min;
     }
 
-    // System.out.println(String.format("=============== (%d, %d) to (%d, %d)\n"+this, startX,
-    // startY, endX, endY));
+    private void recursiveChamber(boolean grid[][], int startX, int startY, int endX, int endY) {
 
-    boolean horizontal;
-    if (chamberSizeX < chamberSizeY) horizontal = true;
-    else if (chamberSizeY < chamberSizeX) horizontal = false;
-    else horizontal = rand.nextInt(3) + 1 == 1;
+        int chamberSizeX = endX-startX + 1;
+        int chamberSizeY = endY-startY + 1;
+        if (chamberSizeX == 2 && chamberSizeY == 2)
+            return;
+        if (chamberSizeX < 2 || chamberSizeY < 2) {
+            // System.out.println(String.format("Aborting (%d, %d) to (%d, %d)", startX, startY, endX, endY));
+            return;
+        }
+            
+        // System.out.println(String.format("=============== (%d, %d) to (%d, %d)\n"+this, startX, startY, endX, endY));
+        
+        boolean horizontal;
+        if (chamberSizeX < chamberSizeY)
+            horizontal = true;
+        else if (chamberSizeY < chamberSizeX)
+            horizontal = false;
+        else
+            horizontal = randInRange(1, 3) == 1;
 
-    boolean wallselected = false;
-    if (horizontal) {
-      // System.out.println("Horizontal wall");
-      int wallY = -1;
-      int tries = 0;
-      while (!wallselected) {
-        if (tries > 100 * chamberSizeY) {
-          // System.out.println(String.format("Couldn't find hwall for (%d, %d) to (%d, %d)",
-          // startX, startY, endX, endY));
-          return;
+        boolean wallselected=false;
+        if (horizontal) {
+            // System.out.println("Horizontal wall");
+            int wallY = -1;
+            int tries = 0;
+            while (!wallselected) {
+                if (tries > 100*chamberSizeY) {
+                    // System.out.println(String.format("Couldn't find hwall for (%d, %d) to (%d, %d)", startX, startY, endX, endY));
+                    return;
+                }
+                    
+                wallY = randInRange(startY+1, endY);
+                if (grid[wallY][startX-1] && grid[wallY][endX+1])
+                    wallselected = true;
+                tries++;
+            }
+                
+            int holeX = randInRange(startX, endX+1);
+            // System.out.println("Hole at x="+holeX);
+            for(int i=0; i<chamberSizeX; i++)
+                grid[wallY][startX+i] = (startX+i != holeX);
+            recursiveChamber(grid, startX, startY, endX, wallY-1);
+            recursiveChamber(grid, startX, wallY+1, endX, endY);
+        } else {
+            // System.out.println("Vertical wall");
+            int wallX = -1;
+            int tries = 0;
+            while (!wallselected) {
+                if (tries > 100*chamberSizeX) {
+                    // System.out.println(String.format("Couldn't find vwall for (%d, %d) to (%d, %d)", startX, startY, endX, endY));
+                    return;
+                }
+                wallX = randInRange(startX+1, endX);
+                if (grid[startY-1][wallX] && grid[endY+1][wallX])
+                    wallselected = true;
+                tries++;
+            }
+            int holeY = randInRange(startY, endY+1);
+            // System.out.println("Hole at y="+holeY);
+            for(int i=0; i<chamberSizeY; i++)
+                grid[startY+i][wallX] = (startY+i != holeY);
+            recursiveChamber(grid, startX, startY, wallX-1, endY);
+            recursiveChamber(grid, wallX+1, startY, endX, endY);
+        }
+        
+    }
+
+    public RecursiveMaze(int width, int height) {
+        this.grid = new boolean[height][width];
+        this.width = width;
+        this.height = height;
+
+        //fill outer walls
+        for(int y=0; y<height; y++) {
+            for(int x=0; x<width; x++) {
+                if (y == 0 || y == height-1 || x == 0 || x == width-1)
+                    grid[y][x] = true;
+            }
         }
 
-        wallY = rand.nextInt(endY - startY) + startY + 1;
-        if (grid[wallY][startX - 1] && grid[wallY][endX + 1]) wallselected = true;
-        tries++;
-      }
+        recursiveChamber(grid, 1, 1, width-2, height-2);
+    }
 
-      int holeX = rand.nextInt(endX + 1 - startX) + startX;
-      // System.out.println("Hole at x="+holeX);
-      for (int i = 0; i < chamberSizeX; i++) grid[wallY][startX + i] = (startX + i != holeX);
-      recursiveChamber(grid, startX, startY, endX, wallY - 1);
-      recursiveChamber(grid, startX, wallY + 1, endX, endY);
-    } else {
-      // System.out.println("Vertical wall");
-      int wallX = -1;
-      int tries = 0;
-      while (!wallselected) {
-        if (tries > 100 * chamberSizeX) {
-          // System.out.println(String.format("Couldn't find vwall for (%d, %d) to (%d, %d)",
-          // startX, startY, endX, endY));
-          return;
+    public char getWidth() {
+        return (char) Integer.toUnsignedLong(width);
+    }
+
+    public char getHeight() {
+        return (char) Integer.toUnsignedLong(height);
+    }
+
+    public int[] emptyPlace() {
+        int tries = 0;
+        while(tries < (width*height)*2) {
+            int x = randInRange(0, width);
+            int y = randInRange(0, height);
+            if (!grid[y][x])
+                return new int[]{x, y};
+            tries++;
         }
-        wallX = rand.nextInt(endX - startX) + startX;
-        if (grid[startY - 1][wallX] && grid[endY + 1][wallX]) wallselected = true;
-        tries++;
-      }
-      int holeY = rand.nextInt(endY + 1 - startY) + startY;
-      // System.out.println("Hole at y="+holeY);
-      for (int i = 0; i < chamberSizeY; i++) grid[startY + i][wallX] = (startY + i != holeY);
-      recursiveChamber(grid, startX, startY, wallX - 1, endY);
-      recursiveChamber(grid, wallX + 1, startY, endX, endY);
-    }
-  }
-
-  public RecursiveMaze(int width, int height) {
-    this.grid = new boolean[height][width];
-    this.width = width;
-    this.height = height;
-
-    // fill outer walls
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        if (y == 0 || y == height - 1 || x == 0 || x == width - 1) grid[y][x] = true;
-      }
+        return null;
     }
 
-    recursiveChamber(grid, 1, 1, width - 2, height - 2);
-  }
+    public int tryMove(int x, int y, int direction, int distance) {
+        int npX = x;
+        int npY = y;
+        int wx = 0;
+        int wy = 0;
+        switch(direction) {
+            case 0:
+                wy = -1;
+                break;
+            case 1:
+                wy = 1;
+                break;
+            case 2:
+                wx = -1;
+                break;
+            case 3:
+                wx = 1;
+                break;
+        }
+        for(int i=0; i<distance; i++) {
+            if (grid[npY+wy][npX+wx])
+                return i;
+            npX += wx;
+            npY += wy;
+        }
 
-  public char getWidth() {
-    return (char) Integer.toUnsignedLong(width);
-  }
-
-  public char getHeight() {
-    return (char) Integer.toUnsignedLong(height);
-  }
-
-  @Override
-  public boolean[][] getSurface() {
-    return grid;
-  }
-
-  public int[] emptyPlace() {
-    int tries = 0;
-    while (tries < (width * height) * 2) {
-      int x = rand.nextInt(width + 1);
-      int y = rand.nextInt(height + 1);
-      if (!grid[y][x]) return new int[] {x, y};
-      tries++;
-    }
-    return null;
-  }
-
-  public int tryMove(int x, int y, int direction, int distance) {
-    int npX = x;
-    int npY = y;
-    int wx = 0;
-    int wy = 0;
-    switch (direction) {
-      case 0:
-        wy = -1;
-        break;
-      case 1:
-        wy = 1;
-        break;
-      case 2:
-        wx = -1;
-        break;
-      case 3:
-        wx = 1;
-        break;
-    }
-    for (int i = 0; i < distance; i++) {
-      if (grid[npY + wy][npX + wx]) return i;
-      npX += wx;
-      npY += wy;
+        return distance;
     }
 
-    return distance;
-  }
-
-  public void draw() {
-    System.out.println(this);
-  }
-
-  public String toString() {
-    String out = "";
-    for (boolean[] line : this.grid) {
-      for (boolean cell : line) {
-        out += cell ? "#" : " ";
-      }
-      out += "\n";
+    public void draw() {
+        System.out.println(this);
     }
-    return out;
-  }
+
+    public String toString() {
+        String out = "";
+        for(boolean[] line : this.grid) {
+            for(boolean cell : line) {
+                out += cell ? "#" : " ";
+            }
+            out += "\n";
+        }
+        return out;
+    }
+
+    public boolean[][] getSurface() {
+      return grid;
+    }
 }
