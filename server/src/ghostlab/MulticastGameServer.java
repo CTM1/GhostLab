1b98b935350825915db8ad1134d56a8a699da5ea
DaRealNim <darealnim@gmail.com>
2022-05-19 09:53:27 +0200

    writeUTF doesn't work

--- server/src/ghostlab/MulticastGameServer.java
+++ server/src/ghostlab/MulticastGameServer.java
@@ -29,10 +29,7 @@ public class MulticastGameServer {
     byte[] content;
     DatagramPacket message;
 
-    ByteArrayOutputStream output = new ByteArrayOutputStream();
-    // nothing but chars with ascii codes under 128, so writing utf is ok
-    (new DataOutputStream(output)).writeUTF(mess);
-    content = output.toByteArray();
+    content = mess.getBytes();
     message = new DatagramPacket(content, content.length, groupeIP, port);
     socket.send(message);
   }
